ffc0a2021fff42b62659c6de53fa60b3075aa97a
Alunara <>
2024-05-12 00:19:31 +0200

    New release

--- assets/index-BakLtyec.js
+++ assets/index-BakLtyec.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Uo,p as K,y as gt}from"./index-D_tDjUb2.js";import"./vendor-D-7xhmkc.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Uo,p as K,y as gt}from"./index-C4j7JiWE.js";import"./vendor-789OdwK6.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
