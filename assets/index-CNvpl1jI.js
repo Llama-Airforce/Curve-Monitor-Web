3766990f9ec626f02ec6c53da07c9777a73b5013
Alunara <>
2024-07-03 15:39:59 +0200

    New release

--- assets/index-CNvpl1jI.js
+++ assets/index-CNvpl1jI.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-CBROaJNy.js";import"./vendor-DQO2c8Qf.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-1irws12D.js";import"./vendor-mcHdxt6F.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
