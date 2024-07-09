4fbb6e6a22aadc1aed221d3d4fb1ac8148408b00
Alunara <>
2024-07-09 14:45:18 +0200

    New release

--- assets/index-C74k-kBw.js
+++ assets/index-C74k-kBw.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-fPjHSy56.js";import"./vendor-Bv8bl_Yn.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-DidqLv5M.js";import"./vendor-Bo8k0Wih.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
