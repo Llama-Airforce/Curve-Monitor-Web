3c50bb20d217b7eb9a3fb0f32052ab30ab173dbf
Alunara <>
2024-08-06 00:07:06 +0200

    New release

--- assets/index-DbViURqv.js
+++ assets/index-DbViURqv.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-C-oa3UVB.js";import"./vendor-BMndS4A2.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-CCHf6E9x.js";import"./vendor-BeZlvf5i.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
