d6ba4d382bd8f5b944594c37e23eb11e5a11e1e4
Alunara <>
2024-08-07 19:32:12 +0200

    New release

--- assets/index-BDzSbjd8.js
+++ assets/index-BDzSbjd8.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-BQaQlTFC.js";import"./vendor-DoQoxwfx.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-DSSsN1ND.js";import"./vendor-DhXnIlGH.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
