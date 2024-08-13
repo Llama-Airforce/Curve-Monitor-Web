752019bc57c6ed604e23e09a31125d78d7489a6b
Alunara <>
2024-08-14 01:40:11 +0200

    New release

--- assets/index-DZvdeWpS.js
+++ assets/index-DZvdeWpS.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-DSSsN1ND.js";import"./vendor-DhXnIlGH.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-B9mbLKA1.js";import"./vendor-Djm-7sNZ.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
