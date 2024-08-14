655690a9b066fa4516ecc33065ba496d31a88209
Alunara <>
2024-08-15 00:23:19 +0200

    New release

--- assets/index-DcIbbB23.js
+++ assets/index-DcIbbB23.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-B9mbLKA1.js";import"./vendor-Djm-7sNZ.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-7gYmvjyX.js";import"./vendor-CzHV0gZt.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
