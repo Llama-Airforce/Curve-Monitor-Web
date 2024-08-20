310ea587384d31c6d32e465963b4c24fdfbe7420
Alunara <>
2024-08-20 02:17:16 +0200

    New release

--- assets/index-DzpTWGyx.js
+++ assets/index-DzpTWGyx.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-CbOy5TWN.js";import"./vendor-BNw4yQJh.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-BWpLxzaE.js";import"./vendor-Cwn8f6OJ.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
