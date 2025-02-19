12463e89edc59ae4d31757b508a467b49803b700
Alunara <>
2025-02-19 22:12:21 +0100

    New release

---
+++ assets/API.page-EE0xHpm_.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Swagger-Cye-S_aF.js","assets/index-aGD7oz-u.js","assets/index-DKFfe5E9.css","assets/Swagger-CzH7p0Cr.css"])))=>i.map(i=>d[i]);
+import{d as o,e as n,o as s,u as r,n as t,p as a}from"./index-aGD7oz-u.js";const u=o({__name:"API.page",setup(p){const e=t(()=>a(()=>import("./Swagger-Cye-S_aF.js"),__vite__mapDeps([0,1,2,3])));return(_,c)=>(s(),n(r(e),{url:"https://prices.curve.fi/feeds-docs/openapi.json"}))}});export{u as default};
