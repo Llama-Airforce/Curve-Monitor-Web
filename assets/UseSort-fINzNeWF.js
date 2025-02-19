9d908fc4ea81d1d74ff6217b935b8fd22cff5a67
Alunara <>
2025-02-19 02:07:46 +0100

    New release

---
+++ assets/UseSort-fINzNeWF.js
@@ -0,0 +1,1 @@
+import{r as t,c as a}from"./index-jdaCXud5.js";function i(e,l){const o=t(e),r=t("desc"),n=(u,c)=>{o.value=u,r.value=c},s=a(()=>({column:o.value,order:r.value}));return{sortColumn:o,sortOrder:r,sorting:s,onSort:n}}export{i as u};
