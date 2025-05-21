2d22c40ec73a2c471a6ceb2329646287f567a714
Alunara <>
2025-05-22 00:09:34 +0200

    New release

--- assets/API.page-ButEzPxL.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Swagger-BIfQNg9e.js","assets/index-D9HG0aMx.js","assets/index-Bc6W-7zj.css","assets/Swagger-CzH7p0Cr.css"])))=>i.map(i=>d[i]);
-import{d as n,h as o,o as s,u as a,n as r,p as t}from"./index-D9HG0aMx.js";const u=n({__name:"API.page",setup(p){const e=r(()=>t(()=>import("./Swagger-BIfQNg9e.js"),__vite__mapDeps([0,1,2,3])));return(_,c)=>(s(),o(a(e),{url:"https://prices.curve.finance/feeds-docs/openapi.json"}))}});export{u as default};
